a064de7d57af77f93e5e2e5a033216e5f386dd2e
Omare Faruk <ofaruk0169@gmail.com>
2023-11-21 20:55:16 +0000

    test

---
+++ app/src/main/java/com/example/stepshabittracker/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.stepshabittracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myStreakMinutes: TextView = findViewById(R.id.streakTimeMinutes)
+
+        // I will now start a time
+        //startTimer()
+    }
+}
