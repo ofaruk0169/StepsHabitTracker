2206404f990db8b85ecefc4fbec64135a3efce3f
Omare Faruk <ofaruk0169@gmail.com>
2023-11-22 22:11:13 +0000

    I have started looking at this YouTube video and learning about how I can implement the initial timer feature

--- app/src/main/java/com/example/stepshabittracker/MainActivity.kt
+++ app/src/main/java/com/example/stepshabittracker/MainActivity.kt
@@ -1,17 +1,65 @@
 package com.example.stepshabittracker
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.stepshabittracker.databinding.ActivityMainBinding
+import java.util.Timer
+import kotlin.math.roundToInt
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var binding: ActivityMainBinding
+    private var timerStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val myStreakMinutes: TextView = findViewById(R.id.streakTimeMinutes)
+        binding.startStopButton.setOnClickListener { startStopTimer() }
+        binding.resetButton.setOnClickListener { resetTimer() }
 
-        // I will now start a time
-        //start the timer here
+        serviceIntent = Intent(applicationContext, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
     }
+
+    private fun resetTimer()
+    {
+        //left off from here yesterday
+    }
+
+    private fun startStopTimer()
+    {
+
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver()
+    {
+        override fun onRecieve(context: Context, intent: Intent)
+        {
+            time = intent.getDoubleExtra(TimerService.TIME_EXTRA, 0.0)
+            binding.streakTimeMinutes.text = getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): String
+    {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 / 3600 / 60
+        val seconds = resultInt % 86400 / 3600 % 60
+
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String = String.format("%02d:%02d:%02d", hour, min, sec)
+
 }
