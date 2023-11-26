e777ae08bece112093268c1f0f61190a9ab98e21
Omare Faruk <ofaruk0169@gmail.com>
2023-11-26 21:23:52 +0000

    I have been learning about looper, handler and message queues for the past 2 hours

--- app/src/main/java/com/example/stepshabittracker/MainActivity.kt
+++ app/src/main/java/com/example/stepshabittracker/MainActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
-
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?)
@@ -15,19 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // as soon as the application is started we need to call, TimerHandler()
-        val timerHandlerClass = TimerHandler()
-        timerHandlerClass.startTimer()
-
         //important buttons that are needed to interact with the application's UI.
         val streakTimeMinutesTextView = findViewById<TextView>(R.id.streakTimeMinutes)
         val changeTextButton = findViewById<Button>(R.id.resetButton)
+
+        // Create a callback to update the UI
+        val updateUICallback: (Int) -> Unit = { currentStreak ->
+            streakTimeMinutesTextView.text = "Current Streak: $currentStreak"
+        }
+
+        val timerHandlerClass = TimerHandler(updateUICallback)
+        timerHandlerClass.startTimer()
+
+
+
         val startStreakButton = findViewById<Button>(R.id.startStopButton)
 
 
-        //this is the button that is used to execute the change in the UI.
+        //this is the button
         changeTextButton.setOnClickListener {
-            streakTimeMinutesTextView.text = "It's changed"
+
         }
     }
 }
--- app/src/main/java/com/example/stepshabittracker/TimerHandler.kt
+++ app/src/main/java/com/example/stepshabittracker/TimerHandler.kt
@@ -2,18 +2,31 @@ package com.example.stepshabittracker
 import android.os.Handler
 import android.os.Looper
 
-class TimerHandler
+class TimerHandler(private val callback: (Int) -> Unit)
 {
     private val mHandler = Handler(requireNotNull(Looper.myLooper()))
+    public var currentStreak: Int = 0
+    private var isTimerRunning: Boolean = false
 
     // the timer should be called as soon as the application is started for the first time
-
-
-
     fun startTimer() {
-        // if the timer is above 00:00:00 this function should not activate again
+        // if the timer is not already running, start the timer
+        if (!isTimerRunning) {
+            isTimerRunning = true
+            mHandler.postDelayed(timerRunnable, 1000) // Delayed by 1 second (1000 milliseconds)
+        }
+        // else the timer should continue as normal.
     }
 
-
-
+    // Runnable to handle the timer logic
+    private val timerRunnable: Runnable = object : Runnable {
+        override fun run() {
+            // Perform timer-related operations here
+            currentStreak++
+            // Log or update UI with the currentStreak value
+            callback.invoke(currentStreak)
+            // Continue the timer by posting the same Runnable with a delay
+            mHandler.postDelayed(this, 1000) // Delayed by 1 second (1000 milliseconds)
+        }
+    }
 }
